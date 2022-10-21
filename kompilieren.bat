4423978ad8f0e79f18a11ac61969e4c06c66626d
Alexander-Ger-Reich <50119493+Alexander-Ger-Reich@users.noreply.github.com>
2022-10-22 01:03:42 +0200

    Update kompilieren.bat

--- kompilieren.bat
+++ kompilieren.bat
@@ -23,44 +23,57 @@ git clone https://github.com/openssl/openssl.git
 cd openssl
 
 @call "C:\Program Files\Microsoft Visual Studio\2022\"%vs_v%"\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
+title 1/8
 perl Configure VC-WIN64A --prefix=%location%\ausgabe\Lib\x64\Release --openssldir=%location%\ausgabe\SSL no-shared
 nmake
 nmake test
 nmake install_sw
 nmake clean
+title 2/8
 perl Configure VC-WIN64A --debug --prefix=%location%\ausgabe\Lib\x64\Debug --openssldir=%location%\ausgabe\SSL no-shared
 nmake
 nmake test
 nmake install_sw
 nmake clean
+title 3/8
 perl Configure VC-WIN64A --prefix=%location%\ausgabe\DLL\x64\Release --openssldir=%location%\ausgabe\SSL
+nmake
 nmake test
 nmake install_sw
 nmake clean
+title 4/8
 perl Configure VC-WIN64A --debug --prefix=%location%\ausgabe\DLL\x64\Debug --openssldir=%location%\ausgabe\SSL
+nmake
 nmake test
 nmake install_sw
 nmake clean
 
 @call "C:\Program Files\Microsoft Visual Studio\2022\"%vs_v%"\VC\Auxiliary\Build\vcvarsall.bat" x86 %*
+title 5/8
 perl Configure VC-WIN32 --prefix=%location%\ausgabe\Lib\x32\Release --openssldir=%location%\ausgabe\SSL no-shared
 nmake
 nmake test
 nmake install_sw
 nmake clean
+title 6/8
 perl Configure VC-WIN32 --debug --prefix=%location%\ausgabe\Lib\x32\Debug --openssldir=%location%\ausgabe\SSL no-shared
 nmake
 nmake test
 nmake install_sw
 nmake clean
+title 7/8
 perl Configure VC-WIN32 --prefix=%location%\ausgabe\DLL\x32\Release --openssldir=%location%\ausgabe\SSL
+nmake
 nmake test
 nmake install_sw
 nmake clean
+title 8/8
 perl Configure VC-WIN32 --debug --prefix=%location%\ausgabe\DLL\x32\Debug --openssldir=%location%\ausgabe\SSL
+nmake
 nmake test
 nmake install_sw
 nmake clean
 
+title Fertig
 echo Fertig
 cmd
